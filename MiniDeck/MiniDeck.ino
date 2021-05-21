b3615834b0e6bb3fea6530552b97e566481f39e6
Uwe Meding <uwe@uwemeding.com>
2021-05-21 15:26:28 -0400

    wip

--- MiniDeck/MiniDeck.ino
+++ MiniDeck/MiniDeck.ino
@@ -8,29 +8,112 @@
 
 #include "Configuration.h"
 
-void setup() {
-  Keyboard.begin();
-
-  ledInit();
-
-  pinMode(KILL_SWITCH_PIN, INPUT);
-
-}
-/*
-   Wait for a key to get pressed, then handle it.
-*/
-void loop()
-{
-  if (digitalRead(KILL_SWITCH_PIN) == HIGH) {
-    //handleKeyStrokes();
-
-    if(digitalRead(MDKx) == HIGH)
-        syncBlink();
-
-  } else {
-    
-    // The kill switch has been triggered. We are in the
-    // safety mode -- just blink
-    alternateBlink();
+int second = 0;
+int checkReset2nd() {
+  int current = second;
+  if (second) {
+    second  = second ^ 1;
+    rxOff();
   }
+  return current;
+}
+int noret = 0;
+int checkNoRet() {
+  return noret;
+}
+
+
+
+void onKey1() {
+  txOn();
+  if (checkReset2nd()) {
+    Keyboard.print(NPASSWORD);
+  } else {
+    Keyboard.print(PASSWORD);
+  }
+  if (!checkNoRet()) {
+    Keyboard.press(KEY_RETURN);
+  }
+  Keyboard.releaseAll();
+  delay(100);
+  txOff();
+}
+void onKey2() {
+  txOn();
+  if (checkReset2nd()) {
+    noret = noret ^ 1;
+  } else {
+    Keyboard.print(USERNAME);
+    Keyboard.press(KEY_TAB);
+  }
+  Keyboard.releaseAll();
+  delay(200);
+  txOff();
+}
+void onKey3() {
+  txOn();
+
+
+
+  
+  delay(200);
+  txOff();
+}
+void onKey4() {
+  txOn();
+  if (checkReset2nd()) {
+    // MS teams: video on/off
+    Keyboard.press(KEY_LEFT_SHIFT);
+    Keyboard.press(KEY_LEFT_CTRL);
+    Keyboard.print("O");
+  } else {
+    // MS teams: mute on/off
+    Keyboard.press(KEY_LEFT_SHIFT);
+    Keyboard.press(KEY_LEFT_CTRL);
+    Keyboard.print("M");
+  }
+  Keyboard.releaseAll();
+  delay(200);
+  txOff();
+}
+
+
+char *greeting[] = {
+  "best,",
+  "Thanks,",
+  "Thanks",
+  "regards,",
+  "Tx,",
+  "thx,"
+};
+
+void onKey5() {
+  txOn();
+  if (checkReset2nd()) {
+    int pos = random(0, sizeof(greeting) / sizeof(char*));
+    char *msg = greeting[pos];
+    Keyboard.print(msg);
+    Keyboard.press(KEY_RETURN);
+  } else {
+    Keyboard.print("Let me know if you have any questions,\n\n");
+    Keyboard.print("Thanks,");
+  }
+  Keyboard.press(KEY_RETURN);
+  Keyboard.releaseAll();
+  Keyboard.print("Uwe");
+  Keyboard.releaseAll();
+
+  delay(100);
+
+  txOff();
+}
+
+void onKey6() {
+  second = second ^ 1;
+  if (second) {
+    rxOn();
+  } else {
+    rxOff();
+  }
+  delay(200);
 }
--- MiniDeck/Utils.ino
+++ MiniDeck/Utils.ino
@@ -1,4 +1,66 @@
+/*
+   Utilities
+*/
+#include "Configuration.h"
 
+
+// ==========================================================
+// Setup the mini deck
+//
+void setup() {
+  Keyboard.begin();
+
+  // get some random analog noise as seed
+  randomSeed(analogRead(0));
+  
+  ledInit();
+
+  pinMode(KILL_SWITCH_PIN, INPUT);
+
+  pinMode(MDK1, INPUT);
+  pinMode(MDK2, INPUT);
+  pinMode(MDK3, INPUT);
+  pinMode(MDK4, INPUT);
+  pinMode(MDK5, INPUT);
+  pinMode(MDK6, INPUT);
+}
+
+// ==========================================================
+// Loop + modes
+//
+#define NORMAL 1
+#define KEY_ID 2
+
+#define RUNMODE NORMAL
+/*
+   Wait for a key to get pressed, then handle it.
+*/
+void loop()
+{
+  if (digitalRead(KILL_SWITCH_PIN) == HIGH) {
+
+#if RUNMODE == NORMAL
+    if (digitalRead(MDK1) == HIGH) onKey1();
+    if (digitalRead(MDK2) == HIGH) onKey2();
+    if (digitalRead(MDK3) == HIGH) onKey3();
+    if (digitalRead(MDK4) == HIGH) onKey4();
+    if (digitalRead(MDK5) == HIGH) onKey5();
+    if (digitalRead(MDK6) == HIGH) onKey6();
+#endif
+
+#if RUNMODE == KEY_ID
+    // identify keys
+    if (digitalRead(MDKx) == HIGH)
+      syncBlink();
+#endif
+
+  } else {
+
+    // The kill switch has been triggered. We are in the
+    // safety mode -- just blink
+    alternateBlink();
+  }
+}
 
 // --- LED helpers
 void ledInit() {
@@ -16,6 +78,18 @@ void ledOn() {
   digitalWrite(LED_BUILTIN, LOW);
   digitalWrite(RXLED, LOW);
   digitalWrite(TXLED, LOW);
+}
+void txOn() {
+  digitalWrite(TXLED, LOW);
+}
+void txOff() {
+  digitalWrite(TXLED, HIGH);
+}
+void rxOn() {
+  digitalWrite(RXLED, LOW);
+}
+void rxOff() {
+  digitalWrite(RXLED, HIGH);
 }
 
 int BLINK_STATE = 0;
